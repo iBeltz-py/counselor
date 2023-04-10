0af7959a5e09209a1f27f4f19838cd847e3cd075
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 14:06:16 +0200

    testing automatized tasks

--- counselor/settings.py
+++ counselor/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.core.management.base import BaseCommand
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,7 +162,11 @@ LOGIN_URL = "/login/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 CRONJOBS = [
-    ('*/1 * * * *', 'tasks.cron.my_scheduled_job')
+    ('*/1 * * * *', 'django.core.management.call_command', ['my_custom_command']),
 ]
+
+# Define the custom command in a management/commands directory
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("Hello from my custom command")
