000049d0351b23d853fa3d2b85587fdc34f39f58
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 12:44:07 +0200

    testing automatized tasks

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 #poetry install
 pip install -r requirements.txt
-
+pip freeze > requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py runcrons
