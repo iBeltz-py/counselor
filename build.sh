e2ba0adf9066da3545f0bfbb5c76f368e33ee8a1
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 13:01:48 +0200

    testing automatized tasks

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 #poetry install
-python manage.py migrate django_cron
 pip install -r requirements.txt
 pip freeze > requirements.txt
+python manage.py migrate django_cron
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py runcrons
