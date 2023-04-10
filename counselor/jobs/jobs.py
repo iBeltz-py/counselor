692f3fddcc7241fbd95a55c4046c17c67b516cb9
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 15:34:11 +0200

    Automatic job working

--- counselor/jobs/jobs.py
+++ counselor/jobs/jobs.py
@@ -1,6 +1,11 @@
 from django.conf import settings
-
+from django.contrib.auth.models import User
+import random
 
 
 def schedule_api():
-    print("hi")
+    users = User.objects.all()
+    for user in users:
+        if random.randint(0,10) == 5:
+            print(user.username)
+    
