7bf372a2d617f032b5a8b0cd025ccbc8a52b0bc2
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 13:48:44 +0200

    testing automatized tasks

--- counselor/settings.py
+++ counselor/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django-crontab.CronJobMiddleware',
+    'django_crontab.CronJobMiddleware',
 
 ]
 
