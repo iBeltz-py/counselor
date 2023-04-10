ca5ddd2097ecbc1b9f8fd2cb88ff92a6468008a1
iBeltz-py <aritzzjl@gmail.com>
2023-04-10 18:32:37 +0200

    Automatic job working

--- counselor/jobs/jobs.py
+++ counselor/jobs/jobs.py
@@ -19,6 +19,7 @@ def schedule_api():
     
 def assign_task(user):
     user = User.objects.filter(username = user)[0]
+    uid = User.objects.filter(username = user)
     print(user)
     userdesc = ""
     hehe = 0
@@ -109,8 +110,9 @@ def assign_task(user):
     ) 
     title = response.choices[0].message.content 
     print(title)     
+    uid = User.objects.get(pk=uid)
     new_task = Task(
-        title = title, description=task, bygpt = True, user = user.username
+        title = title, description=task, bygpt = True, user = uid
     )
     new_task.save()  
     #tasks = Task.objects.filter(user = user.username, datecompleted__isnull=True)
--- counselor/jobs/updater.py
+++ counselor/jobs/updater.py
@@ -4,5 +4,10 @@ from .jobs import schedule_api
 
 def start():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(schedule_api, 'interval', minutes = 3)
+	scheduler.add_job(schedule_api, 'interval', minutes = 1)
 	scheduler.start()
+ 
+ 
+ 
+ 
+ 
